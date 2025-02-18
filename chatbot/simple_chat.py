9be01d3b00264e43e40f8c70d2b79bf3f373aefa
Emeli Dral <emeli.dral@gmail.com>
2025-02-18 13:33:52 +0700

    simple bot simulation

---
+++ chatbot/simple_chat.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import random
+import time
+
+def response_generator():
+    response = "really long test string"
+    for word in response.split(" "):
+        yield word + " "
+        time.sleep(0.1)
+
+st.title("Simple chat")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+prompt = st.chat_input("What is up?")
+if prompt:
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        response = st.write_stream(response_generator())
+    st.session_state.messages.append({"role": "assistant", "content": response})
